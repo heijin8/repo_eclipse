9b1287295e2f167e3ffaf3b1c60b0e9c9b460c95
heijin <2540544168@qq.com>
2018-08-09 11:20:48 +0800

    修改

--- src/main/java/com/heijin/User.java
+++ src/main/java/com/heijin/User.java
@@ -4,22 +4,5 @@ public class User {
 	private String name;
 	private int age;
 	private String address;
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
+	
 }
