fdb2f9cb92a8fc54c2828b3178e356a6d5edd139
heijin <2540544168@qq.com>
2018-08-09 16:11:10 +0800

    又一次提交

--- src/main/java/com/heijin/User.java
+++ src/main/java/com/heijin/User.java
@@ -6,6 +6,36 @@ public class User {
 	private String address;
 	private String id;
 	private String id2;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getId2() {
+		return id2;
+	}
+	public void setId2(String id2) {
+		this.id2 = id2;
+	}
 	
 
 	
